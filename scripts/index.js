5d73204df19ee6707d13c8a95da33b9df5a1e731
Romnick Sarmiento <135184315+s4rmier@users.noreply.github.com>
2023-07-22 07:38:23 +0800

    form UI -ok

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+// let modalVisible = true;
+// let isImportant = false;
+
+// function taskSave() {
+//   console.log("Saved");
+// }
+
+// function toggleModal() {
+//   if (modalVisible) {
+//     $(".task-form").fadeOut(200);
+//     modalVisible = false;
+//   } else {
+//     $(".task-form").fadeIn(200);
+//     modalVisible = true;
+//   }
+// }
+
+// function toggleImportant() {
+//   const importantIcon = "fa-solid";
+//   const notImportantIcon = "fa-regular";
+
+//   if (isImportant) {
+//     $("#icon-important").addClass(notImportantIcon).removeClass(importantIcon);
+//     isImportant = false;
+//   } else {
+//     $("#icon-important").addClass(importantIcon).removeClass(notImportantIcon);
+//     isImportant = true;
+//   }
+// }
+
+// function init() {
+//   console.log("Initialize");
+
+//   $("#button-submit").click(taskSave);
+//   $("#icon-important").click(toggleImportant);
+//   $("#add-task").click(toggleModal);
+// }
+
+// window.onload = init;

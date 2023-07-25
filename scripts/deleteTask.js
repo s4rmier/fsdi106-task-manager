c4151c5de3901b74d9860d49908037aba8317444
Romnick Sarmiento <135184315+s4rmier@users.noreply.github.com>
2023-07-26 07:57:21 +0800

    server-side delete -ok

--- scripts/deleteTask.js
+++ scripts/deleteTask.js
@@ -1,12 +1,32 @@
 function deleteTask(taskId) {
   taskArr.forEach((element, index) => {
     if (taskId == element.id) {
+      objectToDelete = element._id;
+      console.log(objectToDelete);
+
       if (confirm("Are you sure you want to delete this task?")) {
-        taskArr.splice(index, 1);
-        taskPanel.html("");
-        renderElement(taskArr);
-        renderTaskCount(taskArr);
+        const url = `https://fsdiapi.azurewebsites.net/api/tasks/${objectToDelete}/`;
+
+        $.ajax({
+          type: "DELETE",
+          url: url,
+          success: function (response) {
+            console.log(
+              `Deleting task with ID ${taskId} and object ID ${element._id}`
+            );
+            taskPanel.html("");
+            taskArr = [];
+            getData();
+          },
+          error: function (error) {
+            console.log(error);
+          },
+        });
+        // taskArr.splice(index, 1);
       }
+      taskPanel.html("");
+      renderElement(taskArr);
+      renderTaskCount(taskArr);
     }
   });
 }
